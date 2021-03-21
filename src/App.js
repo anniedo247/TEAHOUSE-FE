4dd3123dbcb67fd02ce4325c609bac9b171dc890
Do Thi Hai Van <annie.do247@gmail.com>
2021-03-22 02:29:25 +0700

    set up app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import {Container} from "react-bootstrap"
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1 style={{fontFamily:"'Dancing Script', cursive"}}>Welcome to TeaHouse</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
